dcce331a4183c6d0f3cf5458f1757611c21188a3
Vankadari <nvvankad@amazon.com>
2020-10-12 20:47:42 -0500

    google api key - values masked

--- WebApplication/app.py
+++ WebApplication/app.py
@@ -5,6 +5,8 @@ import os
 import logging
 
 app = Flask(__name__)
+app.config['API_KEYS'] = './API_KEYS/'
+app.config['AUDIO_FILES'] = './static/wav/'
 
 # This part is needed for the showTestLabel function. When we open text files in flask, unlike the Jinja HTML, it isn't smart enough to know that it is in the current flask directory. So you can't specify relative paths and expect it to know what you're talking about. When you type: 'static/true_label/...' it's not going to look for the static folder inside the current directory, it is going to look for it like this 'C:\static\true_label'. It looks at absolute paths, and thus won't find anything. Here we are defining the absolute path in such a way that anybody running this code will be able to use it without problem
 
@@ -32,14 +34,26 @@ def showTestLabel (id) :
 @app.route('/googleAPI<id>', methods=['POST'])
 def showGoogle (id) :
     if request.method == 'POST' :
+        words, speakerIds, randScore = google_api(id)
         return render_template("index.html", Audios = Audios, audio_files = audio_files, id = int(id), which = "google")
 
 @app.route('/amazonAPI<id>', methods=['POST'])
 def showAmazon (id) :
     if request.method == 'POST' :
-        return render_template("index.html", Audios = Audios, audio_files = audio_files, id = int(id), which = "amazon")
+        words, speakerIds, randScore = amazon_api(id) 
+        return render_template("index.html", Audios = Audios, audio_files = audio_files, id = int(id), words, speakerIds, randScore)
 
 @app.route('/azureAPI<id>', methods=['POST'])
 def showAzure (id) :
     if request.method == 'POST' :
         return render_template("index.html", Audios = Audios, audio_files = audio_files, id = int(id), which = "azure")
+
+
+def google_api(id):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(app.config['API_KEYS'],'Google_Api_Key.json')
+    audio_file_name = "interaction" + id + ".wav"
+    audio_file_path = os.path.join(app.config['AUDIO_FILES'], audio_file_name)
+
+
+def amazon_api(id):
+
