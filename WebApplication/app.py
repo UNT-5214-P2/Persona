1922eaa9727caaf1c192449affe4a9b78ab359ad
NicolasStencel <nicolasstencel@my.unt.edu>
2020-10-12 23:00:23 -0500

    new code

--- WebApplication/app.py
+++ WebApplication/app.py
@@ -24,19 +24,24 @@ def index() :
     return render_template("index.html", Audios = Audios, audio_files = audio_files)
     # when you render template remember to import it with 'from flask import render_template'
 
+@app.route('/get_rand', methods=['GET'])
+def get_rand() :
+    return render_template("what_is_rand.html")
+
 @app.route('/testlabel<id>', methods=['POST'])
 def showTestLabel (id) :
     if request.method == 'POST':
-    # when you use request remember to import it
-        with open(TRUE_LABEL_FOLDER + '/true_label' + id + '.txt', "r") as f:
-            content = f.read()
-        
         word_file_name = "words_" + id + ".txt"
         words = os.path.join(app.config['TRUE_LABEL'], word_file_name)
+        with open(words, "r") as f:
+            content_words = f.read()
+
         speaker_ids_file_name = "speaker_id_" + id + ".txt"
         speakerIds = os.path.join(app.config['TRUE_LABEL'], speaker_ids_file_name)
+        with open (speakerIds, "r") as f:
+            content_speakerIds = f.read()
 
-        return render_template("index.html", Audios = Audios, audio_files = audio_files, id = int(id), which = "true", content = content) # id is a string taken from the url, we have to convert to int in order to compare it to loop.index in the html
+        return render_template("index.html", Audios = Audios, audio_files = audio_files, id = int(id), which = "true", words = content_words, speakerIds = content_speakerIds) # id is a string taken from the url, we have to convert to int in order to compare it to loop.index in the html
         
 @app.route('/googleAPI<id>', methods=['POST'])
 def showGoogle (id) :
@@ -48,7 +53,8 @@ def showGoogle (id) :
 def showAmazon (id) :
     if request.method == 'POST' :
         words, speakerIds, randScore = amazon_api(id) 
-        return render_template("index.html", Audios = Audios, audio_files = audio_files, id = int(id), words, speakerIds, randScore)
+        # words, speakerIds, randScore - add this to render_template down below
+        return render_template("index.html", Audios = Audios, audio_files = audio_files, id = int(id))
 
 @app.route('/azureAPI<id>', methods=['POST'])
 def showAzure (id) :
@@ -62,5 +68,4 @@ def google_api(id):
     audio_file_path = os.path.join(app.config['AUDIO_FILES'], audio_file_name)
 
 
-def amazon_api(id):
-
+#def amazon_api(id):
